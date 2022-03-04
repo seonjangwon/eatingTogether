a12ff0cd9bd423eb6eb22c56fad25195863237a5
zzz1won <92144729+zzz1won@users.noreply.github.com>
2022-03-04 15:44:28 +0900

    salePrint

--- src/main/java/com/et/eatingtogether/controller/StoreController.java
+++ src/main/java/com/et/eatingtogether/controller/StoreController.java
@@ -254,14 +254,28 @@ public class StoreController {
 
 
     //아 증말 죄송합니다, 이건 메뉴리스트(업체용)이에요
-    @GetMapping("/MenuControl/{storeNumber}")
-    public String storeMenuControl(@PathVariable Long storeNumber, Model model) {
+    @GetMapping("/menuControl/{storeEmail}")
+    public String storeMenuControl(@PathVariable String storeEmail, Model model) {
         // storeNumber들고 MenuList띄웁니다
-        StoreDetailDTO storeDetailDTO = ss.findByNumber(storeNumber);
-        List<MenuDTO> menuList = ss.menuFindAll(storeNumber);
-        model.addAttribute("storeDetailDTO",storeDetailDTO);
+        StoreDetailDTO storeDetailDTO = ss.findById(storeEmail);
+        /*StoreDetailDTO storeDetailDTO = ss.findByNumber(storeNumber);*/
+        List<MenuDTO> menuList = ss.menuFindAll(storeDetailDTO.getStoreNumber());
+        model.addAttribute("store",storeDetailDTO);
         model.addAttribute("menuList",menuList);
         System.out.println("메뉴리스트 띄우기");
         return "store/MenuControl";
+    }
+
+    @GetMapping ("/sale/{storeEmail}")
+    public String salePage(@PathVariable String storeEmail, Model model)    {
+        //판매 내역 출력
+        StoreDetailDTO store = ss.findById(storeEmail);
+        model.addAttribute("store",store);
+        List<OrderDTO> order = ss.findOrderAll(storeEmail);
+        model.addAttribute("order",order);
+        List<DailySaleDTO> sale = ss.findSaleAll(store.getStoreNumber());
+        model.addAttribute("sale",sale);
+        System.out.println("판매내역 출력할 것");
+        return "store/sale";
     }
 }
