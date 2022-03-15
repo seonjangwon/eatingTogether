cff8332bba7bab170bc9294a6aa00af85f869323
seonjoangwon <wkddnjs740@naver.com>
2022-03-15 14:06:46 +0900

    notebook path add 20220315

--- src/main/java/com/et/eatingtogether/service/StoreServiceImpl.java
+++ src/main/java/com/et/eatingtogether/service/StoreServiceImpl.java
@@ -143,8 +143,9 @@ public class StoreServiceImpl implements StoreService {
         String menuFilename = menuFile.getOriginalFilename();
         menuFilename = System.currentTimeMillis() + menuFilename;
 
-        String savePath = "C:\\Users\\exo_g\\Documents\\GitHub\\eatingTogether\\src\\main\\resources\\static\\upload\\store\\" + menuFilename; // 지원이꺼 + menuFilename;
+//        String savePath = "C:\\Users\\exo_g\\Documents\\GitHub\\eatingTogether\\src\\main\\resources\\static\\upload\\store\\" + menuFilename; // 지원이꺼 + menuFilename;
         /*String savePath = "C:\\Users\\wkddn\\Desktop\\wkddnjs\\eatingTogether\\src\\main\\resources\\static\\upload\\store\\" + menuFilename;*/
+        String savePath = "C:\\development\\source\\FinalProject\\eatingTogether\\src\\main\\resources\\static\\upload\\store\\" + menuFilename;
         if (!menuFile.isEmpty()) {
             menuFile.transferTo(new File(savePath));
         }
